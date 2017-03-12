9aebb11352e948c4e61e3a67293842b96629da8d
Geoff Butler <geoffbutler@users.noreply.github.com>
2017-03-12 16:17:27 +1300

    Bump versions

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Foo.Mixfile do
 
   def project do
     [app: :fun_elixir,
-     version: "0.1.0",
-     elixir: "~> 1.3",
+     version: "0.1.1",
+     elixir: "~> 1.4.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -18,8 +18,8 @@ defmodule Foo.Mixfile do
   end
 
   defp deps do
-    [
-      {:mix_test_watch, "~> 0.1.1"}
+    [			
+			{:mix_test_watch, "~> 0.1.2"}
     ]
   end
 end
